3768493274c3fcfe511a6afbc2da97157f02a288
shabnomnom <shabnam.shomail@gmail.com>
2022-11-18 00:19:36 -0800

    pagination added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import os
 import stat
 import pwd
+import math
 
 
 app = Flask(__name__)
@@ -16,13 +17,34 @@ def validate_folder_path(folder_path):
         raise Exception(f"{folder_path} Not a valid directory")
 
 
-def directory_info(directory):
+def directory_info(directory, page=0):
     """Helper function to extract stat info from directories"""
 
     directory_dict = {}
+    # converting the directory object to a list 
     list_directory = list(os.listdir(directory))
+    print(list_directory)
+    list_directory.sort(key =lambda a : a.lower())
+    #returning 5 items in each page 
+    page_count = math.ceil(len(list_directory) / 5)
+    if page >= page_count:
+        abort(404,f"invalid page number try something from 0 to {page_count}")
 
-    for file in list_directory:
+    start_index_directories = page * 5
+    last_page = page_count -1
+    if page != last_page:
+        end_index = start_index_directories + 5
+    else:
+        end_index = start_index_directories + len(list_directory) % 5
+    page_info = {}
+    page_info['page_info'] = {
+            "total_page_number" : page_count,
+            "current_page": page,
+            "page_size": 5 
+        }
+    
+
+    for file in list_directory[start_index_directories:end_index]:
         file_info = {}
         file_full_path = os.path.join(directory, file)
         file_stat = os.stat(file_full_path)
@@ -32,13 +54,21 @@ def directory_info(directory):
         file_info['owner'] = pwd.getpwuid(file_stat.st_uid).pw_name
         file_info['size'] = file_stat.st_size
         directory_dict[file] = file_info
-    return jsonify(directory_dict)
+    page_info['directories'] = directory_dict
+
+    return jsonify(page_info)
 
 
 @app.route("/")
 def root_index():
     """list all directories and files"""
-    return directory_info(folder_path)
+    # make page a query parameter for user to request diffrent pages 
+    # page_var = x, 2 
+    # len the list_directory
+    # return 0ed index items from 5th to 10th 
+    page_index = request.args.get("page", default = 0, type = int)
+
+    return directory_info(folder_path, page_index)
 
 
 @app.route("/<path:user_path>", methods=['GET'])
@@ -46,9 +76,11 @@ def return_content(user_path):
     """return content of sub-path """
     # create a path from user input
     path = os.path.join(folder_path, user_path)
+    page_index = request.args.get("page", default = 0, type = int)
+
 
     if os.path.isdir(path):
-        return directory_info(path)
+        return directory_info(path, page_index)
     elif os.path.isfile(path):
         file_dict = {}
         with open(path, 'r') as file:
@@ -62,9 +94,11 @@ def return_content(user_path):
 @app.route("/", methods=['POST'])
 @app.route("/<path:user_path>", methods=['POST'])
 def create_new_directory(user_path=""):
-    """create a new drectory and throw error ifalready exist"""
+    """create a new directory and throw error if already exist"""
+
     request_body = request.get_json()
     parent_path = os.path.join(folder_path, user_path)
+
     # validate the parent directory exist
     if not os.path.exists(parent_path):
         abort(400, f'{parent_path} directory does not exist.')
